436967faf0161fdb78e08832a3e7b291407c236d
Roman Pateichuk <roman.shady@yandex.ru>
2021-03-10 23:33:28 +0300

    feat: task is done

--- src/index.js
+++ src/index.js
@@ -1,12 +1,24 @@
 
 exports.min = function min (array) {
-  return 0;
+  if(arguments.length === 0 || array.length === 0){
+    return 0;
+  }
+    return Math.min(...array);
 }
 
 exports.max = function max (array) {
-  return 0;
+  if(arguments.length === 0  || array.length === 0){
+    return 0;
+  }
+    return Math.max(...array);
+  
 }
 
 exports.avg = function avg (array) {
-  return 0;
+  if(arguments.length === 0  || array.length === 0){
+    return 0;
+  }
+   let sum = array.reduce((a, b) => (a + b));
+  return sum/array.length;
 }
+
